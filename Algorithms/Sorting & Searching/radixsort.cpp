ab53f645953b8c8ef885e1e54f0c4afeb3b3d8aa
Maheshwar <75472016+Maheshwar01@users.noreply.github.com>
2021-10-17 22:17:58 +0530

    Update radixsort.cpp

--- Algorithms/Sorting & Searching/radixsort.cpp
+++ Algorithms/Sorting & Searching/radixsort.cpp
@@ -1,8 +1,14 @@
+/*
+Name- Maheshwar
+Reference- https://www.programiz.com/dsa/radix-sort
+*/
+
 // Radix Sort in C++ Programming
 
 #include <iostream>
 using namespace std;
 
+// Function to get the largest element from an array
 int getMax(int array[], int n) {
   int max = array[0];
   for (int i = 1; i < n; i++)
@@ -11,6 +17,7 @@ int getMax(int array[], int n) {
   return max;
 }
 
+// Using counting sort to sort the elements in the basis of significant places
 void countingSort(int array[], int size, int place) {
   const int max = 10;
   int output[size];
@@ -19,12 +26,15 @@ void countingSort(int array[], int size, int place) {
   for (int i = 0; i < max; ++i)
     count[i] = 0;
 
+  // Calculate count of elements
   for (int i = 0; i < size; i++)
     count[(array[i] / place) % 10]++;
 
+  // Calculate cumulative count
   for (int i = 1; i < max; i++)
     count[i] += count[i - 1];
 
+  // Place the elements in sorted order
   for (int i = size - 1; i >= 0; i--) {
     output[count[(array[i] / place) % 10] - 1] = array[i];
     count[(array[i] / place) % 10]--;
@@ -34,14 +44,17 @@ void countingSort(int array[], int size, int place) {
     array[i] = output[i];
 }
 
+// Main function to implement radix sort
 void radixsort(int array[], int size) {
-
+  // Get maximum element
   int max = getMax(array, size);
+
+  // Apply counting sort to sort elements based on place value.
   for (int place = 1; max / place > 0; place *= 10)
     countingSort(array, size, place);
 }
 
-
+// Print an array
 void printArray(int array[], int size) {
   int i;
   for (i = 0; i < size; i++)
@@ -49,7 +62,7 @@ void printArray(int array[], int size) {
   cout << endl;
 }
 
-
+// Driver code
 int main() {
   int array[] = {121, 432, 564, 23, 1, 45, 788};
   int n = sizeof(array) / sizeof(array[0]);
