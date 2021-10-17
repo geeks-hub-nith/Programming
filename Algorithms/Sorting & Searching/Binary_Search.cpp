fe8143d72fbf8230c0afb2599ab1ca09aba70fc3
Sahil-187 <79363174+Sahil-187@users.noreply.github.com>
2021-10-17 19:24:54 +0530

    Added link and information to BinarySearch.cpp

--- Algorithms/Sorting & Searching/Binary_Search.cpp
+++ Algorithms/Sorting & Searching/Binary_Search.cpp
@@ -1,24 +1,30 @@
 /*
     Author :   Sahil (sahil180702@gmail.com)
+    Link : https://www.geeksforgeeks.org/binary-search/
 */
-//Binary Search
-// Time complexity :  O(n) , Space complexity  : O(1)
 
 #include <iostream>
 using namespace std;
+/*
+Binary Search
+Binary Search: Search a sorted array by repeatedly dividing the search interval in half.
+Begin with an interval covering the whole array.
 
+
+Time complexity :  O(n) , Space complexity  : O(1)
+*/
 
 template <class T>
 bool binary_search(T *a,T key,int low,int high){
     int mid;
-    while (low<=high)
+    while (low<=high) // it is the  base condition to terminate the loop 
     {
         mid = (low+high)/2;
-        if(a[mid]==key) return true; 
-        if(a[mid]<key) low = mid+1;
-        else high = mid-1;
+        if(a[mid]==key) return true; // if value at mid of array is equal to key , then we return true and function stop executing.
+        if(a[mid]<key) low = mid+1; // if value at mid is less then the value of key then we proceed with the second half of the array.
+        else high = mid-1; // if value at mid is greater then the value of key then we proceed with the first half of the array.
     }
-    return false;
+    return false; // by default we return false (if key is not found in given internval)
 }
 
 int main(){
