555f3fc5e21cef97ec193a72f2db598a64c49680
Anurag Tripathi <69354224+triipaathii@users.noreply.github.com>
2021-10-19 09:20:51 +0530

    Explanation added

--- Algorithms/Arrays/kadaneAlgorithm.cpp
+++ Algorithms/Arrays/kadaneAlgorithm.cpp
@@ -8,17 +8,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    // Enter the array's size
     int n;
     cin>>n;
+
+    // MAX variable denotes the maximum sum of subarray observed in whole process
     int arr1[n],currentSum=0,MAX=INT_MIN;
+
+    // Enter elemets of the array
     for(int i=0;i<n;i++){
         cin>>arr1[i];
     }
     for(int i=0;i<n;i++){
         currentSum+=arr1[i];
+
+        // if currentSum becomes less than 0, we start new sum of subarray from the next element
         if(currentSum<0){
             currentSum=0;
         }
+
+        // Updating MAX value with currentSum
         MAX=max(currentSum,MAX);
     }
     cout<<MAX;
