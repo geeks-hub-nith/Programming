7ec4a3baa4732aac82f6a60b305f75bb7c219aa1
stevenanthonyrevo <stevenanthonyrevo@live.com>
2022-10-04 11:11:28 -0400

    Added linked list

---
+++ Algorithms/Lists/linked-list.cpp
@@ -0,0 +1,52 @@
+/*
+ Type: Linked List 
+*/
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+  public: 
+    int p; 		// index
+    Node* next;		// pointer to the next node 
+};
+
+Node* newNode(int p)
+{
+	Node* node = new Node;
+	node->p = p;
+	node->next = nullptr;
+	return node;
+}
+
+Node* constructList()
+{
+	Node* first = newNode(1);
+	Node* second = newNode(2);
+	Node* third = newNode(3);
+
+	Node* head = first;
+	first->next = second;
+	second->next = third;
+
+	return head;
+}
+
+void printList(Node* head)
+{
+	Node* ptr = head;
+	while (ptr)
+	{
+		cout << ptr->p << " -> ";
+		ptr = ptr->next;
+	}
+	cout << "nullptr \n";
+}
+
+int main() 
+{
+  Node *head = constructList();
+  printList(head);
+  return 0;
+}
