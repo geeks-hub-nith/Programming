cf58937dc7543e31ff0dc69d77b5f78b9eb072b7
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-10-15 14:07:17 -0400

    Update 282A.cpp

--- CP Solutions/Codeforces/282A.cpp
+++ CP Solutions/Codeforces/282A.cpp
@@ -5,16 +5,16 @@ Author: TinyCodersDen
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int x = 0;
-    int n;
+    int x = 0; // Main answer variable
+    int n; // Number of test cases
     cin >> n;
     for (int t = 0; t<n; t++){
         string k;
         cin >> k;
-        if (k=="++X" || k=="X++"){
+        if (k=="++X" || k=="X++"){ // Possibilities of adding to a variable
             x+=1;
         }
-        else{
+        else{ // Otherwise it's subtraction
             x-=1;
         }
     }
